ed68c4b3bc32d2825aadc3005d56e0b342862477
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 23:01:55 +0300

    07

---
+++ task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное " + num);
+}
+else
+{
+    Console.WriteLine("Нечетное " + num);
+}
