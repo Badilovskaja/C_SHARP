34938c4db36a2a72afaee7f01db58ddb31dbb065
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 23:11:31 +0300

    03

---
+++ task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.WriteLine("ВВедите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// for (int i = -number; i <= number; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// .................или
+
+
+Console.WriteLine("ВВедите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number * -1;
+while (count <= number)
+{
+    Console.Write(count);
+    Console.Write(" ");
+    count++;
+}
+
+
