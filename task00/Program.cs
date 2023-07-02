2b7bc6d0a7c883de0903878037bc3fc735df3ea7
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 23:03:00 +0300

    00

---
+++ task00/Program.cs
@@ -0,0 +1,11 @@
+﻿/*напишите число которое на вход принимает число и выдает его квадрат(число умножение само на себя
+4 -> 16
+-3 -> 9
+-7 -> 49*/
+Console.WriteLine("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
+// 00 Квадрат числа
+
