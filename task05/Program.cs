e74db793ab9583edf65b2241444d44f966d86bf4
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 22:01:01 +0300

    05

---
+++ task05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+    Console.WriteLine($"max = `{numA}`");
+else
+{
+    Console.WriteLine($"max = `{numB}`");
+}
+
