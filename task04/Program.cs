eb61ea588ea96bb4e79f50db3bb05c13b3012b80
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 23:14:35 +0300

    04

---
+++ task04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int integer = 0;
+if (number < 100 || number > 999) Console.WriteLine("Трехзначное число надо!");
+else
+{
+    integer = number % 10;
+    Console.WriteLine(integer);
+}
