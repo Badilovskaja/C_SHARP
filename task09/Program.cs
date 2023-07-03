a4cbf2563f804975e178f5ae626d5704604e1251
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-03 20:41:30 +0300

    09

---
+++ task09/Program.cs
@@ -0,0 +1,32 @@
+﻿// напишите програму которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+// например 78 ->8   12  ->2  85> 5
+
+// int rundomNumber = new Random().Next(10, 100);
+// Console.WriteLine(rundomNumber);
+// int numberOne = rundomNumber % 10;
+// int numberTwo = rundomNumber / 10;
+// if (numberOne > numberTwo)
+// {
+//     Console.WriteLine(numberOne);
+// }
+// else
+// {
+//     Console.WriteLine(numberTwo);
+// }
+
+
+// ................... или
+// Write- пишет в строку
+int rundomNumber = new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число - {rundomNumber}");
+int numberOne = rundomNumber % 10;
+int numberTwo = rundomNumber / 10;
+Console.Write($"У числа {rundomNumber} большая цифра ");
+if (numberOne > numberTwo)
+{
+    Console.WriteLine(numberOne);
+}
+else
+{
+    Console.WriteLine(numberTwo);
+}
