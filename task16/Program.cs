59193bb0efafbc21682ef612f568a117a42f9266
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-05 23:50:16 +0300

    16

---
+++ task16/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if ((num1 * num1 == num2) || (num2 * num2 == num1))
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
