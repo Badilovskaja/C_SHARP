1eea7d185f1a87558b70c13069940bf2f1b4a8da
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-05 22:49:32 +0300

    12

---
+++ task12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите 1 число - ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число - ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber % secondNumber;
+if (result == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine($" остаток {result}");
+}
