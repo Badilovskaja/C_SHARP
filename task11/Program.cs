871d3e4a31e18e9ac3bf48ced3639a612b14ba97
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-05 22:32:23 +0300

    11

---
+++ task11/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberRanndom = new Random().Next(100, 1000);
+
+int numberTwo = numberRanndom % 10;
+int numberThree = numberRanndom / 100;
+
+int result = numberThree * 10 + numberTwo;
+
+Console.WriteLine(numberRanndom);
+Console.WriteLine(result);
