cafe8e2b6cd0900104c493185f814adeb97ef475
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-08 23:28:12 +0300

    18

---
+++ task18/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void GetQuarter(int quarter)
+{
+    if (quarter == 1)
+    {
+        Console.WriteLine("X > 0 && Y > 0");
+    }
+    else if (quarter == 2)
+    {
+        Console.WriteLine("X < 0 && Y > 0");
+    }
+    else if (quarter == 3)
+    {
+        Console.WriteLine("X < 0 && Y < 0");
+    }
+    else if (quarter == 4)
+    {
+        Console.WriteLine("X > 0 && Y < 0");
+    }
+}
+GetQuarter(number);
