1bdffe2f6327b013e16604f2ad9dc1d4662ab378
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-09 18:24:23 +0300

    22

---
+++ task22/Program.cs
@@ -0,0 +1,49 @@
+﻿//написать програму которая принимает на вход число (n) и выдает таблицу квадратов чисел от i до N
+// Console.WriteLine("Введите число");
+// int Num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// int mult = 0;
+// if (Num > 0)
+// {
+//     while (count < Num + 1)
+//     {
+//         mult = count * count;
+//         count = count + 1;
+//         Console.Write($"{mult}, ");
+//     }
+// }
+// else
+// {
+//     count = count * (-1);
+//     while (count > Num - 1)
+//     {
+//         mult = count * count;
+//         count = count - 1;
+//         Console.Write($"{mult}, ");
+//     }
+// }
+
+
+// ...........или с For
+string Squares(int n)
+{
+    string squares = "1";
+    if (n > 2)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            squares = $"{squares} {i * i}";
+        }
+    }
+    else
+    {
+        for (int i = 0; i >= n; i--)
+        {
+            squares = $"{squares} {i * i}";
+        }
+    }
+    return squares;
+}
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Squares(number));
