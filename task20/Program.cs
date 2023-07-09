453fab01e1105b7fbcb7e3f6bd5f70f4f15eb752
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-09 11:44:20 +0300

    200

--- task20/Program.cs
+++ task20/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Поставить две точки в плоскости и узнать растояние между ними
+Console.WriteLine("Enter X for first point");
+int aX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y for first point");
+int aY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter X for second point");
+int bX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y for second point");
+int bY = Convert.ToInt32(Console.ReadLine());
+
+int a = aY - bY;
+int b = aX - bX;
+double distance = Math.Sqrt(a * a + b * b);
+Console.WriteLine(distance);
