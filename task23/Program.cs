18dbc6bac0d09cba7e6be3767b43c34b14109fd9
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-09 20:07:01 +0300

    23

---
+++ task23/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int mult = 0;
+if (Num > 0)
+{
+    while (count < Num + 1)
+    {
+        mult = count * count * count;
+        count++;
+        Console.Write($"{mult}, ");
+    }
+}
+else
+{
+    count = count * (-1);
+    while (count > Num - 1)
+    {
+        mult = count * count * count;
+        count++;
+        Console.Write($"{mult}, ");
+    }
+}
+
+
+
+// Console.WriteLine("Введите число: ");
+// int cNum = Convert.ToInt32(Console.ReadLine());
+
+// void Cube(int[] cNum)
+// {
+//     int counter = 0;
+//     int length = cNum.Length;
+//     while (counter < length)
+//     {
+//         cNum[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+//         counter++;
+//     }
+// }
+
+// void PrintArry(int[] coll)
+// {
+//     int count = coll.Length;
+//     int index = 0;
+//     while (index < count)
+//     {
+//         Console.Write(coll[index] + " ");
+//         index++;
+//     }
+// }
+
+// int[] arry = new int[cNum + 1];
+// Cube(arry);
+// PrintArry(arry);
