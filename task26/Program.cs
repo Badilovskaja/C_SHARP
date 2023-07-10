396c07c5075e06a5e7378b650bac4a544f06bdea
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-10 23:01:51 +0300

    26

---
+++ task26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать функцию принимающую число и выдающую количество цифр в числе
+int AmountOfDigits(int num)
+{
+    int amount = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        amount++;
+    }
+    return amount;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе {number} = {AmountOfDigits(number)}");
