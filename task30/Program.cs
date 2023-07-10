5abdbfb84df519118dca6280f0f322c80a7d29f6
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-11 00:12:50 +0300

    30

---
+++ task30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите прогу которая выводит массив из 8 элементов заполненый  0 i 1
+// 1.... пишем функцию которая выводит масив
+// 2......создаем масив из рандомногочисла 0 и 1
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(2);
+}
+PrintArray(array);
