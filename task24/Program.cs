7a0864eb85e5f31f116e4aae2c96f715436726cc
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-10 22:21:01 +0300

    24

---
+++ task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// ................напишите програму принимающую на вход число(а) и выдающую сумму чисел  от 1 до (а)
+
+
+int GetSumOfSeries(int from, int to)
+{
+    int result = 0;
+    for (int i = from; i <= to; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = GetSumOfSeries(1, number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
