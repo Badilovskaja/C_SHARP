4ecd9278ab38d49fd94c36893607a723ad3e857b
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-10 23:25:24 +0300

    28

---
+++ task28/Program.cs
@@ -0,0 +1,15 @@
+﻿//Написать програму принимаюшую число (А) и выводящее произведение чисел от 1 до (А)
+// Факториал числа А! = 1*2*3*4...А
+int Base(int from, int to)
+{
+    int product = 1;
+    for (int i = from; i <= to; i++)
+    {
+        product *= i;
+    }
+    return product;
+}
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int result = Base(1, value);
+Console.WriteLine($"ответ - {result}");
