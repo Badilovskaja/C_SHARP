585276f0f043606f13e1c52fe0cb822763169017
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-12 05:32:17 +0300

    27

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11 82 -> 10 9012 -> 12
+
+
+int SumX(int numX)
+{
+
+    int count = Convert.ToString(numX).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        advance = numX - numX % 10;
+        result = result + (numX - advance);
+        numX = numX / 10;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+int sumX = SumX(numX);
+Console.WriteLine(sumX);
+
