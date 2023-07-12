4984022605178639904ca1950aa1ca83134672e9
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-12 06:05:23 +0300

    25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 2, 4 -> 16
+
+
+int numMult(int numFirst, int numSecond)
+{
+
+    int count = 1;
+    for (int i = 1; i <= numSecond; i++)
+    {
+        count = count * numFirst;
+    }
+    return count;
+}
+Console.Write("Введите первое число: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numY = Convert.ToInt32(Console.ReadLine());
+
+int result = numMult(numX, numY);
+Console.WriteLine(result);
