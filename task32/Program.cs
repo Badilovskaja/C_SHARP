e3061232b21f2144793705294aeeb011ae076e2c
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-15 16:36:44 +0300

    32

---
+++ task32/Program.cs
@@ -0,0 +1,46 @@
+﻿//Напишите програму замены элементов масива б положительные меняем на отрицательные
+
+//  метод создания рандомного масива
+int[] FillArray(int size, int deviation)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    PrintArray(result);
+    return result;
+}
+
+
+// метод вывода на экран
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.WriteLine($"{array[i]}]");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+}
+
+// метод замены позитивных цифер на негативные
+
+void NegToPos(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] *= -1;
+    }
+}
+
+//выводим значение
+int[] numArray = FillArray(15, 10);
+NegToPos(numArray);
+PrintArray(numArray);
