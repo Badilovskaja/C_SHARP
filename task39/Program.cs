b5e848afb2fa8c02357bc90eecd14f678b364dd9
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-28 11:42:17 +0300

    39

--- task39/Program.cs
+++ task39/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//39 Напишите программу которая перевернет масив последнее число поставит вперед, 1 -будет последним и тд.
+
+// ///................метод создания массива
+int[] GenerateArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+
+}
+// .............метод печати масива
+void printArray(int[] array, string name = "")
+{
+    Console.WriteLine(name);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+// .............метод возвращающего массива
+int[] reversArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    return result;
+}
+
+
+
+// вывод методов
+Console.Clear();
+int[] numbers = GenerateArray(10, -100, 100);
+printArray(numbers, "Начальный");
+int[] revArray = reversArray(numbers);
+printArray(revArray, "перевернытый");
