86dfb8ce954df5d7496d0750d925062d128fc1b7
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-28 20:16:00 +0300

    42

---
+++ task42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая будет преобразовывать десятичные числа в дваичные
+
+
+Console.WriteLine("введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int binary = 0;
+int shift = 1;
+while (value != 0)
+{
+    binary += value % 2 * shift;
+    shift *= 10;
+    value /= 2;
+}
+Console.Write(binary);
