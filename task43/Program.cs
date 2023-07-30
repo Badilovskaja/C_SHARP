42a2ad51f12071c928d85ed64328e0eb0cfecc57
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-30 14:04:52 +0300

    43

--- task43/Program.cs
+++ task43/Program.cs
@@ -4,37 +4,22 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-Console.Write("Введите количество элементов массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] Array = new int[m];
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-void mas(int m)
-{
-    for (int i = 0; i < m; i++)
-    {
-        Console.WriteLine($"Введите {i + 1}элемент массива");
-        Array[i] = Convert.ToInt32(Console.ReadLine());
-    }
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
 
-}
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
 
-int kol(int[] Array)
-{
-    int i = 0;
-    int sum = 0;
-    while (i < Array.Length)
-    {
-        if (Array[i] > 0)
-            sum = sum + 1;
-        i = i + 1;
-    }
-    return sum;
-}
-
-mas(m);
-Console.Write($"\n Чисел больше нуля: {kol(Array)}");
-
-
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
 // Console.Write("Введите k1: ");
 // var k1 = Convert.ToDouble(Console.ReadLine());
